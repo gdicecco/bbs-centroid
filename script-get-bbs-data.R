9e041af912a3b40e145f0c56b66fa33645b3560d
unknown <gdicecco@Bio-HurlbertL02-XPS.ad.unc.edu>
2017-09-06 16:03:41 -0400

    Calculate centroid over 5 year time windows for 1 spp.

--- script-get-bbs-data.R
+++ script-get-bbs-data.R
@@ -9,14 +9,34 @@ weather <- read.csv("\\\\Bioark.bio.unc.edu\\hurlbertlab\\Databases\\BBS\\2017\\
 huang_species <- read.csv("\\\\Bioark.bio.unc.edu\\hurlbertlab\\DiCecco\\huang-2017-bbs-spp.txt", header = TRUE, sep = "\t")
 
 #Remove routes weather runtype = 0
-routes$stateroute <- routes$countrynum*100000 + routes$statenum*1000 + routes$route
-weather$stateroute <- weather$countrynum*100000 + weather$statenum*1000 + weather$route
+routes$stateroute <- routes$statenum*1000 + routes$route
+weather$stateroute <-weather$statenum*1000 + weather$route
 RT1 <- subset(weather, runtype == 1, select = c("stateroute", "year"))
 RT1.routes <- merge(RT1, routes[ , c("stateroute", "latitude", "longitude")], by = "stateroute", all.x = TRUE)
-counts$stateroute <- counts$countrynum*100000 + counts$statenum*1000 + counts$route
+counts$stateroute <- counts$statenum*1000 + counts$route
 
 #for 1st spp- all speciestotal values by route/lat/lon with weather runtype = 1
 spp1 <- subset(counts, aou == huang_species$ID[1], select = c("stateroute", "year", "aou", "speciestotal"))
 spp1.routes <- merge(spp1, RT1.routes[,c("stateroute", "latitude","longitude")], by = "stateroute", all.x = TRUE)
 
-#next need to average routes over 5 year windows for time range used in Huang paper
+#Average routes for 1 spp over five year time windows, calculate centroid
+#install.packages("dplyr")
+#install.packages("DBI")
+#install.packages("lazyeval")
+library("dplyr")
+
+years <- seq(1966, 2016, by = 5)
+results <- matrix(nrow = 10, ncol = 4)
+for (i in 1:10) {
+yr <- years[i]  
+results[i,1] <- yr
+bobwhite <- distinct(filter(spp1.routes, year >= yr & year <= yr + 4, aou == 2890))
+bobwhite.mean <- bobwhite %>%
+  group_by(stateroute, latitude, longitude) %>%
+  summarize(mean_abund = mean(speciestotal, na.rm = TRUE))
+results[i, 4] <- mean(bobwhite.mean$mean_abund)
+results[i,2] <- sum(bobwhite.mean$latitude*bobwhite.mean$mean_abund, na.rm=TRUE)/sum(bobwhite.mean$mean_abund, na.rm=TRUE)
+results[i,3] <- sum(bobwhite.mean$longitude*bobwhite.mean$mean_abund, na.rm=TRUE)/sum(bobwhite.mean$mean_abund, na.rm = TRUE)
+}
+bobwhite.centroid <- data.frame(year = results[,1], centroid.lat = results[,2], centroid.lon = results[,3], mean_abund = results[,4])
+
