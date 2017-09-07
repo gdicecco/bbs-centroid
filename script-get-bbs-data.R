405eec90f04721350fe7a92510fe37614d4a1ddf
unknown <gdicecco@Bio-HurlbertL02-XPS.ad.unc.edu>
2017-09-07 15:46:19 -0400

    for loop to do centroid shift for each spp, results of analysis

--- script-get-bbs-data.R
+++ script-get-bbs-data.R
@@ -15,28 +15,51 @@ RT1 <- subset(weather, runtype == 1, select = c("stateroute", "year"))
 RT1.routes <- merge(RT1, routes[ , c("stateroute", "latitude", "longitude")], by = "stateroute", all.x = TRUE)
 counts$stateroute <- counts$statenum*1000 + counts$route
 
-#for 1st spp- all speciestotal values by route/lat/lon with weather runtype = 1
-spp1 <- subset(counts, aou == huang_species$ID[1], select = c("stateroute", "year", "aou", "speciestotal"))
-spp1.routes <- merge(spp1, RT1.routes[,c("stateroute", "latitude","longitude")], by = "stateroute", all.x = TRUE)
-
-#Average routes for 1 spp over five year time windows, calculate centroid
+#Average routes for each spp over five year time windows, calculate centroid, record shifted distance, velocity, shift bearing, population change
 #install.packages("dplyr")
 #install.packages("DBI")
 #install.packages("lazyeval")
+#install.packages("geosphere")
 library("dplyr")
+library("geosphere")
 
 years <- seq(1966, 2016, by = 5)
-results <- matrix(nrow = 10, ncol = 4)
-for (i in 1:10) {
-yr <- years[i]  
-results[i,1] <- yr
-bobwhite <- distinct(filter(spp1.routes, year >= yr & year <= yr + 4, aou == 2890))
+final <- matrix(nrow = 35, ncol = 5)
+
+setwd("C:/Users/gdicecco/Documents/bbs-centroid/results")
+
+for(i in 1:35) {
+  species <- huang_species[i,1]
+  final[i,1] <- species
+
+  spp1 <- subset(counts, aou == species, select = c("stateroute", "year", "aou", "speciestotal"))
+  spp1.routes <- merge(spp1, RT1.routes[,c("stateroute", "latitude","longitude")], by = "stateroute", all.x = TRUE)
+
+  results <- matrix(nrow = 10, ncol = 4) 
+for (j in 1:10) {
+yr <- years[j]  
+results[j,1] <- yr
+bobwhite <- distinct(filter(spp1.routes, year >= yr & year <= yr + 4, aou == species))
 bobwhite.mean <- bobwhite %>%
   group_by(stateroute, latitude, longitude) %>%
   summarize(mean_abund = mean(speciestotal, na.rm = TRUE))
-results[i, 4] <- mean(bobwhite.mean$mean_abund)
-results[i,2] <- sum(bobwhite.mean$latitude*bobwhite.mean$mean_abund, na.rm=TRUE)/sum(bobwhite.mean$mean_abund, na.rm=TRUE)
-results[i,3] <- sum(bobwhite.mean$longitude*bobwhite.mean$mean_abund, na.rm=TRUE)/sum(bobwhite.mean$mean_abund, na.rm = TRUE)
+results[j, 4] <- mean(bobwhite.mean$mean_abund)
+results[j,2] <- sum(bobwhite.mean$latitude*bobwhite.mean$mean_abund, na.rm=TRUE)/sum(bobwhite.mean$mean_abund, na.rm=TRUE)
+results[j,3] <- sum(bobwhite.mean$longitude*bobwhite.mean$mean_abund, na.rm=TRUE)/sum(bobwhite.mean$mean_abund, na.rm = TRUE)
 }
 bobwhite.centroid <- data.frame(year = results[,1], centroid.lat = results[,2], centroid.lon = results[,3], mean_abund = results[,4])
+write.csv(bobwhite.centroid, paste("AOU_", species, "_centroid_five_year_windows.csv"))
 
+shift <- distGeo(bobwhite.centroid[1,3:2], bobwhite.centroid[10,3:2])/1000
+bring <- bearing(bobwhite.centroid[1,3:2], bobwhite.centroid[10,3:2])
+velocity <- shift/((bobwhite.centroid[10,1]+4)-bobwhite.centroid[1,1])
+delta_abund <- bobwhite.centroid[10,4] - bobwhite.centroid[1,4]
+
+final[i, 2] <- shift
+final[i, 3] <- velocity
+final[i, 4] <- bring
+final[i, 5] <- delta_abund
+}
+
+final.df <- data.frame(aou = final[,1], shifted_dist = final[,2], velocity = final[,3], bearing = final[,4], population_change = final[,5])
+write.csv(final.df, 'centroid_shifts_all_spp.csv')
