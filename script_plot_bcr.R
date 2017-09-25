55596988939e42f0257f72ed95a74a5e63d1415c
gdicecco <31256832+gdicecco@users.noreply.github.com>
2017-09-25 14:23:33 -0400

    Delete script_plot_bcr.R
    Don't need this script any more

--- script_plot_bcr.R
+++
@@ -1,6 +0,0 @@
-library(rgdal)
-bcrshp <- readOGR("C:/Users/gdicecco/Documents/bcr_terrestrial_shape/BCR_Terrestrial_master.shp")
-longs = c(-125,-60)
-lats = c(26,50)
-
-plot(bcrshp, ylim = lats, xlim = longs)
